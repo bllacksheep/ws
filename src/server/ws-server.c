35f7160909fe5ecd3f218c892948241742dde030
b0bul <maclighiche@gmail.com>
2025-09-14 11:26:34 +0100

    update: tcp server layer accept conn

--- src/server/ws-server.c
+++ src/server/ws-server.c
@@ -4,7 +4,7 @@
 #include <sys/socket.h>
 #include <netinet/ip.h>
 
-#define LISTEN_BACKLOG 5
+#define LISTEN_BACKLOG 1
 #define PORT 443
 
 int
@@ -24,19 +24,25 @@ int
 main(int argc, char *argv[]) {
 
     unsigned int sfd;
-    struct sockaddr_in addr;
+    struct sockaddr_in server;
+    struct sockaddr_in client;
     struct in_addr ip;
 
-    if (argc == 2) {
-        if (validate_addr(argv[1]) == -1) {
-            fprintf(stderr, "invalid ip: %s\n", argv[1]);
-        };
-        // what type / format is INADDR_LOOPBACK?
-        ip.s_addr = argv[1];
-    } else if (argc > 2) {
-        fprintf(stderr, "too many arguments\n");
-        return 1;
-    } else {
+    // if (argc == 2) {
+    //     if (validate_addr(argv[1]) == -1) {
+    //         fprintf(stderr, "invalid ip: %s\n", argv[1]);
+    //     };
+    //     // what type / format is INADDR_LOOPBACK?
+    //     ip.s_addr = argv[1];
+    // } else if (argc > 2) {
+    //     fprintf(stderr, "too many arguments\n");
+    //     return 1;
+    // } else {
+    //     ip.s_addr = htonl(INADDR_LOOPBACK);
+    // }
+    //
+    
+    if (argc < 2) {
         ip.s_addr = htonl(INADDR_LOOPBACK);
     }
 
@@ -46,13 +52,13 @@ main(int argc, char *argv[]) {
         return 1;
     }
 
-    memset(&addr, 0, sizeof(addr));
+    memset(&server, 0, sizeof(server));
 
-    addr.sin_family = AF_INET; 
-    addr.sin_port = htons((unsigned short)PORT);
-    addr.sin_addr = ip;
+    server.sin_family = AF_INET; 
+    server.sin_port = htons((unsigned short)PORT);
+    server.sin_addr = ip;
 
-    if (bind(sfd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+    if (bind(sfd, (struct sockaddr *)&server, sizeof(server)) == -1) {
         fprintf(stderr, "bind error: %s\n", strerror(errno));
         return 1;
     }
@@ -62,7 +68,21 @@ main(int argc, char *argv[]) {
         return 1;
     }
 
+
+    memset(&client, 0, sizeof(client));
+    
+    socklen_t cl = sizeof(client);
     printf("Listening on 127.0.0.1:%d\n", PORT);
+    while (1) {
+        int cfd = accept(sfd, (struct sockaddr *)&client, &cl);
+        if (cfd == -1) {
+            fprintf(stderr, "accept error: %s\n", strerror(errno));
+            return 1;
+        } else {
+            fprintf(stdout, "connect accept on: %d\n", 123);
+        }
+    }
+
 
     return 0;
 }
