0fb11628293850243059ba040741090ef0216626
b0bul <maclighiche@gmail.com>
2025-09-22 16:50:55 +0100

    test: server setup and teardown

--- tests/smoke.c
+++ tests/smoke.c
@@ -1,3 +1,33 @@
 #include <criterion/criterion.h>
+#include <signal.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-Test(misc, failing) { cr_assert(0, "hello"); }
+#define PORT 8080
+#define SERVER "bin/server"
+
+static pid_t server_pid;
+
+void setup(void) {
+  pid_t pid = fork();
+  cr_assert(pid >= 0, "error: fork");
+  if (pid == 0) {
+    execl(SERVER, SERVER, (char *)NULL);
+    perror("error: exec");
+    exit(1);
+  } else {
+    server_pid = pid;
+    sleep(1);
+  }
+}
+
+void teardown(void) {
+  kill(server_pid, SIGTERM);
+  waitpid(server_pid, NULL, 0);
+}
+
+TestSuite(smoke, .init = setup, .fini = teardown, .disabled = 0);
+
+Test(smoke, conn) { cr_assert(1, "hello"); }
