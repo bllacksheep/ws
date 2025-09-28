c9e43c2f29b1af6845eb4a44bd152b12e354ca14
b0bul <maclighiche@gmail.com>
2025-09-28 22:58:28 +0100

    test: basic structure

--- tests/smoke.c
+++ tests/smoke.c
@@ -26,11 +26,15 @@ void setup(void) {
 
 void teardown(void) {
   kill(server_pid, SIGTERM);
-  waitpid(server_pid, NULL, 0);
+  int status;
+  waitpid(server_pid, &status, 0);
+  if (WIFEXITED(status)) {
+    printf("exist status: %d", WEXITSTATUS(status));
+  }
 }
 
 TestSuite(smoke, .init = setup, .fini = teardown, .disabled = 0);
 
 Test(smoke, basic) { cr_assert(1, "hello"); }
 
-Test(smoke, siege) { cr_assert(1, "hello"); }
+// Test(smoke, siege) { cr_assert(1, "hello"); }
