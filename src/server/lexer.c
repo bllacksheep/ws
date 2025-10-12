da71a93a5bc44c98fb793a86b0434f8f6ac8f08d
Jason Leitch <maclighiche@gmail.com>
2025-10-12 15:15:27 +0100

    update: add notion of request stream tokens

--- src/server/lexer.c
+++ src/server/lexer.c
@@ -2,37 +2,39 @@
 #include <string.h>
 
 typedef enum {
-  character,
-  space,
-  slash,
-  carriage,
-  newline,
-  dot,
-  colon,
-} type_t;
+  CHAR,
+  SPACE,
+  SLASH,
+  CARRIAGE,
+  NEWLINE,
+  DOT,
+  COLON,
+} stream_type_t;
+
+typedef enum { METHOD, PATH, VERSION, HEADER } http_type_t;
 
 typedef struct {
   char val;
-  type_t type;
-} token_t;
+  stream_type_t type;
+} stream_token_t;
 
-void tokenize(char *input) {
-  token_t tokens[100];
+void tokenize_request_stream(char *input) {
+  stream_token_t token_stream[100] = {0};
 
   for (int i = 0; i < strlen(input); i++) {
-    token_t token = {0};
+    stream_token_t token;
     if (input[i] == ' ') {
       token.val = input[i];
-      token.type = space;
+      token.type = SPACE;
     } else if (input[i] == '/') {
       token.val = input[i];
-      token.type = slash;
+      token.type = SLASH;
     } else if (input[i] == ':') {
       token.val = input[i];
-      token.type = colon;
+      token.type = COLON;
     }
 
-    tokens[i] = token;
+    token_stream[i] = token;
   }
 }
 
@@ -40,5 +42,5 @@ int main() {
 
   char *req = "GET /chat HTTP/1.1\r\nHost: 127.0.0.1:443\r\nUser-Agent: "
               "curl/7.81.0\r\nAccept: */*\r\n\r\n";
-  tokenize(req);
+  tokenize_request_stream(req);
 }
