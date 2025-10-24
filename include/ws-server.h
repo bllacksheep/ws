2c21b2d5a7e990662a58ab09f9e5e07e8cc17276
Jason Leitch <maclighiche@gmail.com>
2025-10-24 15:35:46 +0100

    update: start implementation of vector as a connection manager

--- include/ws-server.h
+++ include/ws-server.h
@@ -1,4 +1,5 @@
 // rfc 6455
+#include <stdlib.h>
 
 #ifndef _WS_SERVER_h
 #define _WS_SERVER_h
@@ -6,5 +7,17 @@
 #define LISTEN_BACKLOG 20
 #define MAX_EVENTS 10
 #define PORT 443
+#define MAX_BYTE_STREAM_IN 256
+
+typedef struct {
+  int fd;
+  char buf[MAX_BYTE_STREAM_IN];
+} conn_ctx_t;
+
+typedef struct {
+  conn_ctx_t *conn;
+  size_t len;
+  size_t cap;
+} connection_manager_t;
 
 #endif
