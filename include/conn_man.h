416959ffed42d6c53e0abe4896100384f0c7dbc2
Jason Leitch <maclighiche@gmail.com>
2025-11-15 22:33:24 +0000

    fix: cxn -> cnx

--- include/conn_man.h
+++ include/conn_man.h
@@ -1,45 +1,40 @@
 // rfc 6455
 
-#ifndef _CX_MAN_h
-#define _CX_MAN_h
+#ifndef _CNX_MAN_h
+#define _CNX_MAN_h
 
 #include <ctx.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define CX_MANAGER_BYTE_STREAM_IN 1024
-#define CX_MANAGER_CONN_POOL 1024
-#define CX_MANAGER_DEALLOC_THRESHOLD 100
+#define CNX_MANAGER_BYTE_STREAM_IN 1024
+#define CNX_MANAGER_CONN_POOL 1024
+#define CNX_MANAGER_DEALLOC_THRESHOLD 100
 
-enum cxn_state { KEEPALIVE, CLOSE };
-
-typedef struct {
-  http_t *http;
-  ws_t *ws;
-} ctx_t;
+enum cnx_state { KEEPALIVE, CLOSE };
 
 typedef struct {
   int fd;
-  uint8_t *buf[CX_MANAGER_BYTE_STREAM_IN];
-  enum cxn_state reuse;
-  ctx_t ctx;
-} cxn_ctx_t;
+  uint8_t *buf[CNX_MANAGER_BYTE_STREAM_IN];
+  enum cnx_state reuse;
+  http_t *http;
+} ctx_t;
 
 typedef struct {
-  cxn_ctx_t **cxn;
+  ctx_t **cxn;
   size_t len;
   size_t cap;
   size_t allocated;
-} cxn_manager_t;
+} cnx_manager_t;
 
-cxn_manager_t *cxn_manager_create();
+cnx_manager_t *cnx_manager_create();
 // tracks connection
-static void cxn_manager_add(cxn_manager_t *cm, int cfd);
+static void cnx_manager_add(cnx_manager_t *cm, int cfd);
 // check state of conn add / remove as required
-void cxn_manager_track(cxn_manager_t *cm, int cfd);
+void cnx_manager_track(cnx_manager_t *cm, int cfd);
 // releases connection
-void cxn_manager_remove(cxn_manager_t *cm, int cfd);
+void cnx_manager_remove(cnx_manager_t *cm, int cfd);
 // fetches connection
-cxn_ctx_t *cxn_manager_get(cxn_manager_t *cm, int cfd);
+ctx_t *cnx_manager_cnx_context(cnx_manager_t *cm, int cfd);
 
 #endif
