3e61d033072f87a4aece8bfbcb27b054d01e900a
Jason Leitch <maclighiche@gmail.com>
2025-11-25 14:28:35 +0000

    update: basic k,v retrival test

--- src/server/hash_table.c
+++ src/server/hash_table.c
@@ -19,7 +19,7 @@ typedef struct {
   size_t size;
 } ThreadMap;
 
-_Thread_local ThreadMap tls_map = {.epoch = 0};
+_Thread_local ThreadMap tls_map = {.epoch = 0, .size = BUCKET_COUNT};
 
 static inline void map_clear(ThreadMap *m) {
   // set epoch as uint8_t check memset impact vs compiler branch prediction
@@ -33,28 +33,29 @@ static inline void map_clear(ThreadMap *m) {
   //   memset(m->buckets, 0, sizeof(m->buckets));
   //   m->epoch = 1;
   // }
-  m->size = 0;
 }
 
 static inline void new_map() {
   for (int i = 0; i < BUCKET_COUNT; i++) {
     Bucket *bucket = malloc(sizeof(Bucket));
+    bucket->key = NULL;
     bucket->value = NULL;
-    bucket->epoch = 0;
+    bucket->epoch = 1;
     tls_map.buckets[i] = bucket;
   }
 }
 
 static void insert(ThreadMap *m, const uint8_t *k, const uint8_t *v) {
-  int32_t index = ht_get_hash(k, m->size, 0);
+  int32_t index = ht_get_hash(k, 0);
   Bucket *b = m->buckets[index];
 
   int32_t i = 1;
-  while (b != NULL && m->epoch > b->epoch) {
-    index = ht_get_hash(k, m->size, i);
+  while (b != NULL && m->epoch == b->epoch) {
+    index = ht_get_hash(k, i);
     Bucket *b = m->buckets[index];
     i++;
   }
+  b->key = k;
   b->value = v;
   b->epoch++;
   m->buckets[index] = b;
@@ -67,53 +68,12 @@ static const uint8_t *get_map(ThreadMap *m, const uint8_t *k) {
   int32_t i = 1;
   while (1) {
     if (strcmp((const char *)b->key, (const char *)k) == 0) {
-      return k;
+      return b->value;
     }
     i++;
     index = ht_get_hash(k, i);
     Bucket *b = m->buckets[index];
   }
-}
-
-static ht_item *ht_new_item(const uint8_t *k, const uint8_t *v) {
-  ht_item *i = malloc(sizeof(ht_item));
-  // printf("ht item alloc %zuB\n", sizeof(ht_item));
-  i->key = (uint8_t *)strdup((const char *)k);
-  // printf("item k alloc %zuB\n", sizeof(uint8_t) * strlen((const char *)k));
-  i->value = (uint8_t *)strdup((const char *)v);
-  // printf("item v alloc %zuB\n", sizeof(uint8_t) * strlen((const char *)v));
-
-  return i;
-}
-
-ht_hash_table *ht_new() {
-  ht_hash_table *ht = malloc(sizeof(ht_hash_table));
-  // printf("ht alloc %zuB\n", sizeof(ht_hash_table));
-
-  ht->size = HT_MAX_SIZE;
-  ht->count = 0;
-  ht->items = calloc((size_t)ht->size, sizeof(ht_item *));
-
-  // printf("ht items alloc %zuB\n", sizeof(ht_item) * ht->size);
-
-  return ht;
-}
-
-static void ht_del_item(ht_item *i) {
-  free(i->key);
-  free(i->value);
-  free(i);
-}
-
-void ht_del_hash_table(ht_hash_table *ht) {
-  for (int i = 0; i < ht->size; i++) {
-    ht_item *item = ht->items[i];
-    if (item != NULL) {
-      ht_del_item(item);
-    }
-  }
-  free(ht->items);
-  free(ht);
 }
 
 static int32_t ht_hash(const uint8_t *s, const int32_t a, const int32_t m) {
@@ -132,17 +92,9 @@ static int32_t ht_get_hash(const uint8_t *s, const int32_t attempt) {
   return (hash_a + (attempt * (hash_b + 1))) % BUCKET_COUNT;
 }
 
-void ht_insert(ht_hash_table *ht, const uint8_t *key, const uint8_t *value) {
-  ht_item *item = ht_new_item(key, value);
-  int32_t index = ht_get_hash(item->key, 0);
-  ht_item *cur_item = ht->items[index];
-  int32_t i = 1;
-  while (cur_item != NULL) {
-    index = ht_get_hash(item->key, i);
-    cur_item = ht->items[index];
-    i++;
-  }
-  ht->items[index] = item;
-  ht->count++;
-  // printf("k: %s, i: %d, v: %s\n", key, index, value);
+int main() {
+  new_map();
+  insert(&tls_map, (const uint8_t *)"Host", (const uint8_t *)"12324");
+  const uint8_t *v = get_map(&tls_map, (const uint8_t *)"Host");
+  printf("val: %s\n", v);
 }
