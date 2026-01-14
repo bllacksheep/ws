85c08d4ab538d205aaff7394714b1222a6a5f2ef
bllacksheep <maclighiche@gmail.com>
2026-01-14 15:14:37 +0000

    update: move default addr macro

--- include/server.h
+++ include/server.h
@@ -1,10 +1,12 @@
 // rfc 6455
 
 #ifndef _WS_SERVER_h
-#define _WS_SERVER_h
+#define _WS_SERVER_h 1
 
 #define LISTEN_BACKLOG 1024
 #define MAX_EVENTS 1024
-#define PORT 443
+// encryption tbd
+#define DEFAULT_LISTEN_PORT 443
+#define DEFAULT_LISTEN_ADDR INADDR_LOOPBACK
 
 #endif
