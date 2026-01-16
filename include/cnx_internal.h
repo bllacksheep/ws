19ae2117c19277129f6c2dc950548ad05e9781be
bllacksheep <maclighiche@gmail.com>
2026-01-16 12:12:54 +0000

    update: rework manager into allocator

--- include/cnx_internal.h
+++ include/cnx_internal.h
@@ -1,11 +1,22 @@
-enum state {
+#ifndef _CNX_INTERNAL_H
+#define _CNX_INTERNAL_H 1
+#include<sys/types.h>
+
+enum conn_reuse {
   KEEPALIVE = 1,
   CLOSE,
 };
 
+enum conn_state {
+    UNUSED = 0
+    CLOSED,
+    PENDING,
+}
+
 // not connected to protocol or http yet
 typedef struct conn {
-  enum state reuse;
+  enum conn_reuse reuse;
+  enum conn_state state;
   unsigned int fd;
   unsigned int event_loop_fd;
   ssize_t inbuf_n;
@@ -14,3 +25,5 @@ typedef struct conn {
   char *inbuf;
   char *outbuf;
 } cnx_t;
+
+#endif
--- source/conn_man.c
+++ source/conn_man.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 typedef struct conn_man {
   cnx_t **cnx;
@@ -16,41 +17,67 @@ static void cm_add_cnx(cnx_manager_t *, int);
 static void cm_track_cnx(cnx_manager_t *, int);
 static void cm_remove_cnx(cnx_manager_t *, int);
 
+
+static void cm_add_cnx(cm_manage_t *cm) {
+  cm->cnx = (cnx_t **)calloc(CNX_MANAGER_CONN_POOL, sizeof(cnx_t *));
+  if (cm->cnx == NULL) {
+      perror("could not create connections");
+      exit(-1);
+  }
+
+  for (int i = 0; i < CNX_MANAGER_CONN_POOL; i++) {
+    cnx_t *cnx = (cnx_t *)calloc(1, sizeof(cnx_t));
+    if (cnx == NULL) {
+      perror("could not create cnx connection");
+      exit(-1);
+    }
+    cm->cnx[i] = cnx;
+    cm->cnx[i]->reuse = KEEPALIVE;
+    cm->cnx[i]->state = UNUSED;
+    cm->cnx[i]->inbuf = (uint8_t *)calloc(CNX_MANAGER_BYTE_STREAM_SIZEIN, sizeof(uint8_t));
+    if (cm->cnx[i]->inbuf == NULL) {
+      perror("could not create connection inbuf");
+      exit(-1);
+    }
+    cm->cnx[i]->outbuf = (uint8_t *)calloc(CNX_MANAGER_BYTE_STREAM_SIZEOUT, sizeof(uint8_t));
+    if (cm->cnx[i]->outbuf == NULL) {
+      perror("could not create connection outbuf");
+      exit(-1);
+    }
+  }
+}
+
 cnx_manager_t *cm_allocator() {
-  cnx_manager_t *cm = (cnx_manager_t *)malloc(sizeof(cnx_manager_t));
+  cnx_manager_t *cm = (cnx_manager_t *)calloc(1, sizeof(cnx_manager_t));
   if (cm == NULL) {
       perror("could not create connection manager");
       exit(-1);
   }
-  cm->cnx = (cxn_t **)malloc(sizeof(cxn_t *) * CNX_MANAGER_CONN_POOL);
-
-  for (int i = 0; i < CNX_MANAGER_CONN_POOL; i++) {
-    cnx_t *cnx = (cxn_t *)calloc(1, sizeof(cxn_t));
-    cm->cnx[i] = cnx;
-  }
-  cm->len = 0;
+  cm_add_cnx(cm);
   cm->cap = CNX_MANAGER_CONN_POOL;
-  cm->allocated = 0;
+  cm->allocated = CNX_MANAGER_CONN_POOL;
   return cm;
 }
 
-static void cm_add_cnx(cnx_manager_t *cm, int cfd) {
-  if (cm->allocated >= CNX_MANAGER_DEALLOC_THRESHOLD) {
+static void cm_remove_cnx(cnx_t *cnx) {
+    cnx->state = CLOSED;
+}
+
+static void cm_track_cnx(cnx_manager_t *cm, int fd) {
+  if (cm == NULL) {
+      fprintf(stderr, "no connection manager");
+      exit(-1);
   }
-  if (cm->cnx[cfd] == NULL) {
-    // handle
+
+  // check if allocated still enough, error count of some kind?
+  if (cm->cnx[fd]->state == PENDING) {
+      fprintf(stderr, "conn still in use");
+      exit(-1);
   }
-  cm->cnx[cfd]->fd = cfd;
-  cm->cnx[cfd]->reuse = KEEPALIVE;
+
+  cm->cnx[fd]->fd = fd;
+  cm->cnx[fd]->state = PENDING;
   cm->len++;
-  cm->allocated++;
-
-  cm->cnx[cfd]->buf =
-      (uint8_t *)malloc(sizeof(uint8_t) * CNX_MANAGER_BYTE_STREAM_IN);
-
-  if (cm->cnx[cfd]->buf == NULL) {
-    // handle
-  }
 
   cm->cnx[cfd]->http = (http_ctx_t *)malloc(sizeof(http_ctx_t));
   if (cm->cnx[cfd]->http == NULL) {
@@ -111,4 +138,3 @@ static void cm_add_cnx(cnx_manager_t *cm, int cfd) {
   // }
 }
 
-static void cm_remove_cnx(cnx_manager_t *cm, int cfd) {}
