e2c17cc7b500ecce8bbc23ff91025bfa7734646d
bllacksheep <maclighiche@gmail.com>
2026-01-17 14:12:48 +0000

    update: add logger

---
+++ include/log.h
@@ -0,0 +1,18 @@
+#ifndef _LOG_H
+#define _LOG_H 1
+#include <stdarg.h>
+#include <stdio.h>
+
+#define LOG(fmt, ...) logger(__FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
+static void logger(const char* file, int line, const char *fn, const char *fmt, ...);
+
+// external linkage
+static void logger(const char* file, int line, const char *fn, const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    fprintf(stderr, "%s:%d:%s:", file, line, fn);
+    vfprintf(stderr, fmt, ap);
+    fputc('\n', stderr);
+    va_end(ap);
+}
+#endif
