b7729e0d575578be70109b6604341e020b73b4fe
bllacksheep <maclighiche@gmail.com>
2026-01-19 23:20:57 +0000

    update: start to work in LOG

--- source/http.c
+++ source/http.c
@@ -1,4 +1,5 @@
 #include "http.h"
+#include "log.h"
 #include "cnx_internal.h"
 #include "http_internal.h"
 #include <errno.h>
@@ -46,19 +47,19 @@ static void http_handle_raw_request_stream(http_ctx_t *ctx,
   // switch statement here
   // needs response + headers + body
   if (ctx->request->method == UNKNOWN) {
-    fprintf(stderr, "error: initialize request line unknown payload\n");
+    LOG("error: initialize request line unknown payload");
     stream_outbuf_n = strlen(stream_outbuf);
     stream_outbuf = _400_bad_request;
   }
 
   if (strcmp((char *)ctx->request->path, HTTP_ENDPOINT) != 0) {
-    fprintf(stderr, "error: initialize request line uri expectet /chat\n");
+    LOG("error: initialize request line uri expectet /chat");
     stream_outbuf_n = strlen(stream_outbuf);
     stream_outbuf = _400_bad_request;
   }
 
   if (ctx->request->method != GET) {
-    fprintf(stderr, "error: initialize request line not GET method\n");
+    LOG("error: initialize request line not GET method");
     stream_outbuf_n = strlen(stream_outbuf);
     stream_outbuf = _405_method_not_allowed;
   }
@@ -70,42 +71,49 @@ static void http_handle_raw_request_stream(http_ctx_t *ctx,
 http_ctx_t *http_alloc_http_ctx(void) {
   http_ctx_t *ctx = calloc(1, sizeof(*ctx));
   if (ctx == NULL) {
+    LOG("could not alloc http");
     perror("could not alloc http");
     exit(-1);
   }
 
   ctx->request = calloc(1, sizeof(*ctx->request));
   if (ctx->request == NULL) {
+    LOG("could not alloc http request");
     perror("could not alloc http request");
     exit(-1);
   }
 
   ctx->response = calloc(1, sizeof(*ctx->response));
   if (ctx->response == NULL) {
+    LOG("could not alloc http response");
     perror("could not alloc http response");
     exit(-1);
   }
 
   ctx->request->body = calloc(1, sizeof(*ctx->request->body));
   if (ctx->request->body == NULL) {
+    LOG("could not alloc http request body");
     perror("could not alloc http request body");
     exit(-1);
   }
 
   ctx->response->body = calloc(1, sizeof(*ctx->response->body));
   if (ctx->response->body == NULL) {
+    LOG("could not alloc http response body");
     perror("could not alloc http response body");
     exit(-1);
   }
 
   ctx->request->body->buf = calloc(REQUEST_BODY_BUF_SIZE, sizeof(*ctx->request->body->buf));
   if (ctx->request->body->buf == NULL) {
+      LOG("could not alloc http request body buf");
       perror("could not alloc http request body buf");
     exit(-1);
   }
 
   ctx->response->body->buf = calloc(RESPONSE_BODY_BUF_SIZE, sizeof(*ctx->response->body->buf));
   if (ctx->response->body->buf == NULL) {
+    LOG("could not alloc http response body buf");
     perror("could not alloc http response body buf");
     exit(-1);
   }
@@ -147,6 +155,7 @@ void http_handle_incoming_cnx(cnx_t *cx) {
   if (cx->stream_inbuf_n == 0) {
 
     if (epoll_ctl(cx->ev_loop_fd, EPOLL_CTL_DEL, cx->fd, NULL) == -1) {
+      LOG("coudl not remove fd from interest list");
       perror("could not remove fd from interest list");
     }
 
@@ -164,10 +173,12 @@ void http_handle_incoming_cnx(cnx_t *cx) {
       cx->stream_outbuf_written_n +=
           write(cx->fd, cx->stream_outbuf, cx->stream_outbuf_n);
       if (cx->stream_outbuf_written_n == -1) {
+        LOG("could not write on fd");
         perror("could not write on fd");
         return;
       }
       if (cx->stream_outbuf_written_n != (ssize_t)cx->stream_outbuf_n) {
+        LOG("could not partial write on fd");
         fprintf(stderr, "error: partial write on fd: %d, %s\n", cx->fd,
                 strerror(errno));
         return;
